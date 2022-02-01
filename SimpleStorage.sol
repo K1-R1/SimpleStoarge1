47f51b889b3e07f19d1b94a9e1db43c6fa611b40
KR <kyranrawlinson@gmail.com>
2022-02-01 15:15:12 +0000

    Updated README and formatted SimpleStorage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,31 +1,28 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-contract SimpleStorage{
-
+contract SimpleStorage {
     uint256 public favouriteNumber;
 
-    struct People{
+    struct People {
         uint256 favouriteNumber;
         string name;
     }
-    
+
     People[] public people;
-    
+
     mapping(string => uint256) public nameToFavouriteNumber;
 
-    
-    
     function store(uint256 _favouriteNumber) public {
         favouriteNumber = _favouriteNumber;
     }
 
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         return favouriteNumber;
     }
 
-    function addPerson(string memory _name, uint256 _favouriteNumber) public{
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
         people.push(People(_favouriteNumber, _name));
         nameToFavouriteNumber[_name] = _favouriteNumber;
     }
